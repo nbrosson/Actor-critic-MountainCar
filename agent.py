121c3287e53edd38f4d12669ce25ae87f0874448
nbrosson <45031747+nbrosson@users.noreply.github.com>
2019-04-08 08:46:24 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
-import matplotlib.pyplot as plt
+
 """
 Contains the definition of the agent that will run in an
 environment.
@@ -13,16 +13,16 @@ environment.
 class ActorCritic(nn.Module):
     def __init__(self):
         super(ActorCritic, self).__init__()
-        self.affine = nn.Linear(2, 128)
+        self.affine = nn.Linear(2, 256)
         
-        self.action_layer = nn.Linear(128, 2)
-        self.value_layer = nn.Linear(128, 1)
+        self.action_layer = nn.Linear(256, 2)
+        self.value_layer = nn.Linear(256, 1)
         
         self.logprobs = []
         self.state_values = []
         self.rewards = []
         self.actions = []
-        self.all_loss = []
+        
         
 
     def forward(self, observation):
@@ -35,19 +35,29 @@ class ActorCritic(nn.Module):
         observation = observation_as_list
         
         state = torch.from_numpy(observation).float()
-        state = F.tanh(self.affine(state))
-        
-        state_value = self.value_layer(state)
-        weight_prelu = torch.tensor([[5.0]])
-        action_parameters = (self.action_layer(state)) # A voir quelle activation function mettre
+        forward_state = F.tanh(self.affine(state))
+        #forward_state = (self.affine(state))
+
+        state_value = self.value_layer(forward_state)
+        action_parameters = (self.action_layer(forward_state)) # A voir quelle activation function mettre
         action_distribution = Normal(action_parameters[0][0], action_parameters[0][1])
         
         action = action_distribution.sample() # Torch.tensor; action
-        self.logprobs.append(action_distribution.log_prob(action+ 1e-6))
+        action_log_prob = action_distribution.log_prob(action)
+#        while math.isnan(action_log_prob.item()) == True:
+##            forward_state = F.tanh(self.affine(state))
+#             #action_parameters = (self.action_layer(forward_state)) # A voir quelle activation function mettre
+#             action_parameters[0][0] += 0.01
+#             action_parameters[0][1] += 0.01
+#             action_distribution = Normal(action_parameters[0][0], action_parameters[0][1])
+##            action = action_distribution.sample()
+#             print("ca bloque", action)
+#             action_log_prob = action_distribution.log_prob(action)
+#            print(action_distribution)
+        self.logprobs.append(action_log_prob)
         self.state_values.append(state_value)
         #print(action.item())
         return action.item() # Float element
-        
         
     
     def calculateLoss(self, gamma=0.99):
@@ -67,8 +77,11 @@ class ActorCritic(nn.Module):
             
             advantage = reward  - value.item()
             action_loss = -logprob * advantage
+            
             value_loss = F.smooth_l1_loss(value, reward)
+            #print(action_loss)
             loss += (action_loss + value_loss)  
+            
         return loss
     
     def clearMemory(self):
@@ -88,11 +101,11 @@ class RandomAgent():
         
         self.count_episodes = -1
         self.max_position = -0.4
-        self.epsilon = 0.9
+        self.epsilon = 0.5
         self.gamma = 0.99
         self.running_rewards = 0
         self.policy = ActorCritic()
-        self.optimizer = optim.Adam(self.policy.parameters(), lr=0.01, betas=(0.9, 0.999))
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=0.001, betas=(0.9, 0.999))
         self.check_new_episode = 1
         self.count_iter = 0
         self.successful = 0        
@@ -107,12 +120,12 @@ class RandomAgent():
 
         range for vx is always [-20, 20]
         """
-        self.epsilon = (self.epsilon * 0.9)
+        self.epsilon = (self.epsilon * 0.94)
         self.count_episodes += 1
         self.victory = False
-        if self.count_episodes == 99:
-            plt.plot([i for i in range(99)], self.policy.all_loss, 'ro')
-            plt.show()
+        #if self.count_episodes == 99:
+        #    plt.plot([i for i in range(99)], self.policy.all_loss, 'ro')
+        #    plt.show()
             
         
         #return (np.random.uniform(x_range[0],x_range[1]), np.random.uniform(-20,20))
@@ -139,6 +152,9 @@ class RandomAgent():
             return np.random.uniform(-1,1)
         
         else:
+            x = observation[0]
+            vx = int((observation[1]+20)//5)
+            action = (x,vx)
             action = self.policy(observation)
             return action
         
@@ -150,16 +166,18 @@ class RandomAgent():
 
         This is where your agent can learn.
         """
+        
         if reward > 0:
+            print(observation)
             self.successful += 1
             print(self.successful)
             self.policy.rewards.append(reward)
             index = len(self.policy.rewards)
-            for i in range(index-10,index):
-                self.policy.rewards[i] += 50 
+            for i in range(index-12,index):
+                self.policy.rewards[i] += 30
             self.optimizer.zero_grad()
             self.loss = self.policy.calculateLoss(self.gamma)
-            self.policy.all_loss.append(self.loss)
+            #self.policy.all_loss.append(self.loss)
             self.loss.backward()
             self.optimizer.step() 
             self.policy.clearMemory()
@@ -171,9 +189,11 @@ class RandomAgent():
             self.policy.rewards.append(reward)
             if self.count_iter == 400:
                 # We want first to update the critic agent:
+                for i in range(400):
+                    self.policy.rewards[i] -= 10  
                 self.optimizer.zero_grad()
                 self.loss = self.policy.calculateLoss(self.gamma)
-                self.policy.all_loss.append(self.loss)
+                #self.policy.all_loss.append(self.loss)
                 self.loss.backward()
                 self.optimizer.step() 
                 self.policy.clearMemory()
